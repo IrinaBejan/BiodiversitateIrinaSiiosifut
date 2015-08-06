c72f7dc7ec399914b7749d16a05286d4a03b5fc2
Bejan Irina <madalina_bejan2000@yahoo.com>
2015-08-06 10:11:43 +0300

    Init project
    The coolest project on Earth

---
+++ BiodiversitateGalaciuc/Assets/GetQuestionsFromXml.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetQuestionsFromXml : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
